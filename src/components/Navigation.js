c81b5a31cf1a5788e22790cd414ceecf1e842046
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-07 18:58:36 -0400

    added navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import '../App.css';
+import {img} from '../images/logo.jpg';
 
 class Navigation extends Component {
     render() {
       return (
         <div className="Navigation"> 
-            
-
+           <img src ={require("../images/logo.jpg")} alt="Nautilus Logo" />
+            <a href="#">ISSUES</a>
+            <a href="#">TOPICS</a>
+            <a href="#">BLOG</a>
+            <a href="#">NEWSLETTER</a>
+            <a href ="#"><i class="fa fa-facebook-f"></i></a>
+            <a href ="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="#">LOGIN</a>
+            <button id ="subscribe">SUBSCRIBE</button>
         </div>
       );
     }
