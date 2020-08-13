33e75e6f7d5ada4e03fcd2a04ec1766efe65ae6b
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-12 20:39:53 -0400

    added styling to Facts

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -24,21 +24,21 @@ class Facts extends Component {
                     <p className="subject">BIOLOGY</p>
                     <p className="title">Do Butterflies Challenge the Meaning of Species?</p>
                     <p className="description">What is a species? It is a question that has agonized scientists since well before Darwin. With some exceptions, the thinking has landed on an evidently firm reproductive barrier: Members...</p>
-                    <p>READ MORE</p>
+                    <p className="readmore">READ MORE</p>
                 </div>
                 <div>
                     <img src ={require('../images/matter.jpg')} />
                     <p className="subject">MATTER</p>
                     <p className="title">Hologram Within a Hologram Hints at Fate of Black Holes</p>
                     <p className="description">Reprinted with permission from Quanta Magazine's Abstractions blog. Like cosmic hard drives, black holes pack troves of data into compact spaces. But ever since Stephen Hawking calculated...</p>
-                    <p>READ MORE</p>
+                    <p className="readmore">READ MORE</p>
                 </div>
                 <div>
                     <img src ={require('../images/biology_02.jpg')} />
                     <p className="subject">BIOLOGY</p>
                     <p className="title"> Where to See the Real Living Dead</p>
                     <p className="description">Talk of "Mother Trees," from a scientist studying plant life, can sound fanciful, like something out of a fairy tale. Suzanne Simard is here to tell you that it's not. For the past...</p>
-                    <p>READ MORE</p>
+                    <p className="readmore">READ MORE</p>
                 </div>
             </div>  
         </div>
