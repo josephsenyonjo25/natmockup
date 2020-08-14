8138cbc90578acbf338efc31510b89fe17be6bc4
josephsenyonjo25 <67437047+josephsenyonjo25@users.noreply.github.com>
2020-08-13 21:11:31 -0400

    added styling to Prime

--- src/components/Prime.js
+++ src/components/Prime.js
@@ -12,14 +12,16 @@ class Prime extends Component {
            <button id ="members">MEMBERS SIGN IN</button>
            
             <div id ="primecontent">
-                <img src ={require('../images/hp_prime.jpg')} /> 
-                <div>
+                <div id ="primeimage">
+                <img src ={require('../images/hp_prime.jpg')} />
+                </div> 
+                <div id="primetext">
                     <p>Like Nautilus? Get Nautilus Prime.</p>
-                    <p>Unlimited online reading Read it all, whenever, wherever, and however you like.</p>
-                    <p>Ad Free Get a pristine, ad-free reading experience.</p>
-                    <p>Tablet editions All the content and design of our award-winning print magazine, in PDF format --perfect for reading on your tablet or desktop.</p>
-                    <p>eBook editions We've made each monthly online issue into an eBook, compatible with most e-readers.</p>
-                    <p> Event Access Prime members get priority access to Nautilus events.</p>
+                    <p><span className="bold">Unlimited online reading</span> Read it all, whenever, wherever, and however you like.</p>
+                    <p><span className="bold">Ad Free</span> Get a pristine, ad-free reading experience.</p>
+                    <p><span className="bold underline">Tablet editions</span> All the content and design of our award-winning print magazine, in PDF format --perfect for reading on your tablet or desktop.</p>
+                    <p><span className ="bold underline">eBook editions</span> We've made each monthly online issue into an eBook, compatible with most e-readers.</p>
+                    <p><span className ="bold">Event Access</span> Prime members get priority access to Nautilus events.</p>
                     <button id ="learnmore">LEARN MORE</button>
                     <button id ="joinprime">JOIN PRIME</button>
                     <p>Current print subscriber?<br />You're already a Prime member.</p>
